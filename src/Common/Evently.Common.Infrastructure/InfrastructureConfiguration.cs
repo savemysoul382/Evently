80b12927c966c76808f3c8881fba18fba017ec32
savemysoul382 <serjke88@gmail.com>
2026-03-01 11:01:40 +0300

    Redis
    - Distributed caching

--- src/Common/Evently.Common.Infrastructure/InfrastructureConfiguration.cs
+++ src/Common/Evently.Common.Infrastructure/InfrastructureConfiguration.cs
@@ -1,16 +1,19 @@
-﻿using Evently.Common.Application.Clock;
+﻿using Evently.Common.Application.Caching;
+using Evently.Common.Application.Clock;
 using Evently.Common.Application.Data;
+using Evently.Common.Infrastructure.Caching;
 using Evently.Common.Infrastructure.Clock;
 using Evently.Common.Infrastructure.Data;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Npgsql;
+using StackExchange.Redis;
 
 namespace Evently.Common.Infrastructure;
 
 public static class InfrastructureConfiguration
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string databaseConnectionString)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string databaseConnectionString, string redisConnectionString)
     {
         NpgsqlDataSource npgsqlDataSource = new NpgsqlDataSourceBuilder(databaseConnectionString).Build();
         services.TryAddSingleton(npgsqlDataSource);
@@ -18,6 +21,16 @@ public static class InfrastructureConfiguration
         services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
 
         services.TryAddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        IConnectionMultiplexer connectionMultiplexer = ConnectionMultiplexer.Connect(redisConnectionString);
+        services.TryAddSingleton(connectionMultiplexer);
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.ConnectionMultiplexerFactory = () => Task.FromResult(connectionMultiplexer);
+        });
+
+        services.TryAddSingleton<ICacheService, CacheService>();
 
         return services;
     }
